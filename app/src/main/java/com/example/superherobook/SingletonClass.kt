e7354eeb7e6d8f242530b01660774b93435251ad
ebruaeo <ebruaeo@hotmail.com>
2024-02-26 16:58:34 +0300

    Singleton Class oluşturuldu

---
+++ app/src/main/java/com/example/superherobook/SingletonClass.kt
@@ -0,0 +1,10 @@
+package com.example.superherobook
+
+import android.graphics.Bitmap
+
+class SingletonClass {
+    companion object SecilenKahraman {
+        var gorsel: Bitmap? = null
+    }
+
+}
